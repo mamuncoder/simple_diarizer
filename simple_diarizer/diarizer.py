a1b955c3ca48d68720c338d2f0221435222fa092
mamuncoder <81091616+mamuncoder@users.noreply.github.com>
2024-05-02 08:35:14 +0530

    Update diarizer.py

--- simple_diarizer/diarizer.py
+++ simple_diarizer/diarizer.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import torch
 import torchaudio
-from speechbrain.pretrained import EncoderClassifier
+from speechbrain.inference import EncoderClassifier
 from tqdm.autonotebook import tqdm
 
 from .cluster import cluster_AHC, cluster_SC
